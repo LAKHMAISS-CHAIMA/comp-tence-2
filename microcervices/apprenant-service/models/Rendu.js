83b728de948c262204810f7581343bcdc736b729
LAKHMAISS CHAIMA <chaimaalakhmaiss23@gmail.com>
2025-07-07 17:46:31 +0100

    Créer le modèle Rendu

--- microcervices/apprenant-service/models/Rendu.js
+++ microcervices/apprenant-service/models/Rendu.js
@@ -1,1 +1,11 @@
-console.log()
+import mongoose from "mongoose";
+
+const renduSchema = new mongoose.Schema({
+    apprenantId: { type: mongoose.SchemaType.Types.ObjectId, ref: 'Apprenant' },
+    briefId: { type: String, required: true },
+    description: { type: String, required: true },
+    note: { type: Number, min: 0, max: 20 },
+    commentaire: { type: String, trim: true },
+    dateSoumission: Date
+});
+export default mongoose.model('Rendu', renduSchema)
