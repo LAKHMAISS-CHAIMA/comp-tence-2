4da8d1ce9774712d43a4fa68bb00f5a1d8e3d3fc
LAKHMAISS CHAIMA <chaimaalakhmaiss23@gmail.com>
2025-07-07 17:45:19 +0100

    Créer le modèle Apprenant

--- microcervices/apprenant-service/models/Apprenant.js
+++ microcervices/apprenant-service/models/Apprenant.js
@@ -1,1 +1,7 @@
-console.log()
+import mongoose from "mongoose";
+
+const apprenantSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true}
+});
+export default mongoose.model('Apprenant', apprenantSchema)
