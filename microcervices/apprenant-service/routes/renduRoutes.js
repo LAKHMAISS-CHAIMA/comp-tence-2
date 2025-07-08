0b43efbd0608bc32a7e11e9274a740e8b1c09559
LAKHMAISS CHAIMA <chaimaalakhmaiss23@gmail.com>
2025-07-08 11:08:41 +0100

    Créer les routes pour les rendus

--- microcervices/apprenant-service/routes/renduRoutes.js
+++ microcervices/apprenant-service/routes/renduRoutes.js
@@ -1,1 +1,15 @@
-console.log()
+import express from 'express';
+import mongoose from 'mongoose';
+import Rendu from '../models/Rendu.js';
+import renduController from '../controllers/renduController.js';
+
+const router = express.Router();
+
+router.post('/rendus', renduController.createRendu);
+router.get('/rendus', renduController.getAllRendus);
+router.get('/rendus/:id', renduController.getRenduById);
+router.patch('/rendus/:id', renduController.updateRendu);
+router.delete('/rendus/:id', renduController.deleteRendu);
+
+export default router;
+
